30d5bfaf4330f2189ddbfc5fa8a0314be6eee8eb
Francis Nguyen <francis@ngyn.me>
2020-10-20 00:12:48 -0600

    Undo previous USB changes; add USB serial over OTG port

--- br2_external/board/frbncis/pi-crust/post-image.sh
+++ br2_external/board/frbncis/pi-crust/post-image.sh
@@ -15,9 +15,18 @@ disable_splash=1
 __EOF__
     fi
     ;;
+    --add-dwc2-overlay)
+    if ! grep -qE '^dtoverlay=dwc2' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
+      echo "Adding 'dtoverlay=dwc2' to config.txt."
+      cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+dtoverlay=dwc2
+__EOF__
+    fi
+    ;;
 	esac
 
 # TODO: Parse post-image script arguments to set cmdline.txt
-echo "root=/dev/mmcblk0p2 rootwait console=tty1 logo.nologo" > "${BINARIES_DIR}/rpi-firmware/cmdline.txt"
+echo "root=/dev/mmcblk0p2 rootwait console=tty1 console=ttyAMA0,115200 modules-load=dwc2,g_serial logo.nologo" > "${BINARIES_DIR}/rpi-firmware/cmdline.txt"
 
 done
