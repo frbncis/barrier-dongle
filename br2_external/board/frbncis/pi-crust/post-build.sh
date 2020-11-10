523eddcdc300b643a1d6a4f2e0ff98629ac1ed97
Francis Nguyen <francis@ngyn.me>
2020-11-10 04:18:56 +0000

    Add data partition

--- br2_external/board/frbncis/pi-crust/post-build.sh
+++ br2_external/board/frbncis/pi-crust/post-build.sh
@@ -10,10 +10,14 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 ::sysinit:\/sbin\/modprobe brcmfmac # WiFi driver' ${TARGET_DIR}/etc/inittab
 fi
 
-mkdir -p ${TARGET_DIR}/boot
+mkdir -p ${TARGET_DIR}/boot  ${TARGET_DIR}/data
 
 # Mount boot partition as readonly
 if [ -e ${TARGET_DIR}/etc/fstab ]; then
   grep -qE '^/dev/mmcblk0p1.*' ${TARGET_DIR}/etc/fstab || \
     echo '/dev/mmcblk0p1  /boot           auto    ro              0       0' | tee -a ${TARGET_DIR}/etc/fstab
+
+  grep -qE '^/dev/mmcblk0p3.*' ${TARGET_DIR}/etc/fstab || \
+    echo '/dev/mmcblk0p3  /data           auto    rw              0       0' | tee -a ${TARGET_DIR}/etc/fstab
+
 fi
